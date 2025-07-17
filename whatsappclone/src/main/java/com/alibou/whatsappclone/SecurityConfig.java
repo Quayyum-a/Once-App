bc9aab46b02663c1b5dfcebbccde534c0ff1faef
Quayyum <quayyumariyo@gmail.com>
2025-07-17 13:07:44 +0100

    add security filter chain configuration

--- whatsappclone/src/main/java/com/alibou/whatsappclone/SecurityConfig.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/SecurityConfig.java
@@ -1,7 +1,43 @@
 package com.alibou.whatsappclone;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/v3/api-docs/**",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs",
+                                        "/swagger-resources/**",
+                                        "/swagger-resources",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/webjars/**",
+                                        "/swagger-ui.html",
+                                        "/ws/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token  -> token.jwtAuthenticationConverter(new JwtAuthenticationConverter())));
+
+        return http.build();
+    }
+
 }
