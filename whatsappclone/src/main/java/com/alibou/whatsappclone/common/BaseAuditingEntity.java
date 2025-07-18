0be0225a1ac0d50cd831dbc99a4b3605d951c9f6
Quayyum <quayyumariyo@gmail.com>
2025-07-18 13:47:26 +0100

    edit

--- whatsappclone/src/main/java/com/alibou/whatsappclone/common/BaseAuditingEntity.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/common/BaseAuditingEntity.java
@@ -1,5 +1,6 @@
 package com.alibou.whatsappclone.common;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
@@ -7,7 +8,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -17,4 +22,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseAuditingEntity {
+    @CreatedDate
+    @Column(name = "created_date", updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date", insertable = false)
+    private LocalDateTime lastModifiedDate;
 }
