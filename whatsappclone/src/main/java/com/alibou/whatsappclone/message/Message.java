23a94c4b27b9cf11699a8cd1a0a87c6672857600
Quayyum <quayyumariyo@gmail.com>
2025-07-18 15:09:50 +0100

    Created Message class

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/message/Message.java
@@ -0,0 +1,34 @@
+package com.alibou.whatsappclone.message;
+
+import com.alibou.whatsappclone.common.BaseAuditingEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.awt.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "messages")
+public class Message extends BaseAuditingEntity {
+
+    @Id
+    @SequenceGenerator(name = "msg_seq", sequenceName = "msg_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "msg_seq")
+    private Long id;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    @Enumerated(EnumType.STRING)
+    private MessageType type;
+    @Enumerated(EnumType.STRING)
+    private MessageState state;
+    @Column(name = "sender_id", nullable = false)
+    private String senderId;
+    @Column(name = "receiver_id", nullable = false)
+    private String receiverId;
+}
