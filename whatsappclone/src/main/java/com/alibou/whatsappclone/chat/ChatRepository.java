9e9283ab6838b9b2eba7db1b21c5cf83176dbf4f
Quayyum <quayyumariyo@gmail.com>
2025-07-20 17:41:09 +0100

    Add ChatMapper class to convert Chat entities to ChatResponse DTOs

--- whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatRepository.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatRepository.java
@@ -1,6 +1,12 @@
 package com.alibou.whatsappclone.chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, String> {
+    @Query(name = ChatConstants.FIND_CHAT_BY_SENDER_ID)
+    List<Chat> findChatsBySenderId(@Param("senderId") String userId);
 }
