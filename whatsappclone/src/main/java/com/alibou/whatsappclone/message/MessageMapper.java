c8590c4b5647ef4805a4ffa058a3852e5a172b18
Quayyum <quayyumariyo@gmail.com>
2025-07-20 19:33:00 +0100

    Message

--- whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageMapper.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageMapper.java
@@ -4,4 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageMapper {
+
+    public MessageResponse toMessageResponse(Message message) {
+        return MessageResponse.builder()
+                .id(message.getId())
+                .content(message.getContent())
+                .type(message.getType())
+                .state(message.getState())
+                .senderId(message.getSenderId())
+                .receiverId(message.getReceiverId())
+                .createdDate(message.getCreatedDate())
+                // todo read the media file
+                .build();
+    }
+
 }
