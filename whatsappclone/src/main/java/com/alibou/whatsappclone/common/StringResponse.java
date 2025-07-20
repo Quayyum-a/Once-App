8968cb0ad3cf63b2bb0eafca2e5dcbeab232e635
Quayyum <quayyumariyo@gmail.com>
2025-07-20 18:31:00 +0100

    StringResponse

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/common/StringResponse.java
@@ -0,0 +1,13 @@
+package com.alibou.whatsappclone.common;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StringResponse {
+    private String response;
+
+}
