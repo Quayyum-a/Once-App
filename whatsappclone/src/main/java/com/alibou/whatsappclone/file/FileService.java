f0924d6cffbf40574b6ba106a98b90dc1f1e1b89
Quayyum <quayyumariyo@gmail.com>
2025-07-21 14:46:13 +0100

    file

--- whatsappclone/src/main/java/com/alibou/whatsappclone/file/FileService.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/file/FileService.java
@@ -1,10 +1,17 @@
 package com.alibou.whatsappclone.file;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class FileService {
+
+    @Value("application.file.uploads.media-output.path")
+    private String mediaOutputPath;`
     public static String saveFile(MultipartFile file, String senderId) {
         return null;
     }
