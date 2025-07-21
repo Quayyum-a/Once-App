c78f16ed352fec254e534df1805c97ee7d0ec6d8
Quayyum <quayyumariyo@gmail.com>
2025-07-21 04:57:33 +0100

    Message

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/file/FileService.java
@@ -0,0 +1,11 @@
+package com.alibou.whatsappclone.file;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+    public static String saveFile(MultipartFile file, String senderId) {
+        return null;
+    }
+}
